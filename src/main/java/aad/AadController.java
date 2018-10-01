2b0deee130441f980065f0f1e0a9750fa136e3ab
Matt Cadorette <mcadorette@gmail.com>
2018-10-01 18:40:34 -0400

    Initial commit from Atomist

---
+++ src/main/java/aad/AadController.java
@@ -0,0 +1,20 @@
+package aad;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+class AadController {
+
+    @GetMapping("/hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "Hello, world! Add /hello/there to the URL to get a friendly reply.";
+    }
+
+}
